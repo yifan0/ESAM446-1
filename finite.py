11e373e608234e2d5e792a34985668364dc83330
yifan0 <yifan12@illinois.edu>
2023-10-01 13:06:41 -0500

    add non-uniform case

--- finite.py
+++ finite.py
@@ -53,10 +53,8 @@ class DifferenceUniformGrid(Difference):
         for k in range(0,kmax+1):
             for j in range(jmin,jmax+1):
                 S[k,j-jmin] = 1/factorial(k)*(j*grid.dx)**k
+        print(S)
         # solve for a
-        test = np.array([[ 1, 1, 1],
-              [-grid.dx, 0, grid.dx],
-              [grid.dx**2/2, 0, grid.dx**2/2]])
         a = np.linalg.inv(S) @ b
         shape = [grid.N, grid.N]
         stencil = a
@@ -83,4 +81,40 @@ class DifferenceNonUniformGrid(Difference):
         self.convergence_order = convergence_order
         self.stencil_type = stencil_type
         self.axis = axis
+        # set up b
+        if (derivative_order + convergence_order)%2 == 0:
+            b = np.zeros(derivative_order + convergence_order + 1)
+            b[derivative_order] = 1
+            kmax = derivative_order + convergence_order
+            jmin = -int(kmax/2)
+            jmax = int(kmax/2)
+        else:
+            b = np.zeros(derivative_order + convergence_order)
+            b[derivative_order] = 1
+            # k \in [0, kmax]
+            kmax = derivative_order + convergence_order - 1
+            # j \in [jmin, jmax]
+            jmin = -int(kmax/2)
+            jmax = int(kmax/2)
+        # initialize D matrix
+        shape = (grid.N, grid.N)
+        D = sparse.csr_array(shape)
+        # fill out each row of D
+        for i in range(0,grid.N):
+            # S dimensions JXK
+            S = np.zeros((kmax+1,jmax-jmin+1))
+            for k in range(0,kmax+1):
+                for j in range(jmin,jmax+1):
+                    v_i = grid.values[i]
+                    v_ipj = grid.values[(i+j+grid.N)%grid.N]
+                    if i+j < 0:
+                        v_ipj -= grid.length
+                    if i+j > grid.N-1:
+                        v_ipj += grid.length
+                    S[k,j-jmin] = 1/factorial(k)*(v_ipj-v_i)**k
+            # solve for a
+            a = np.linalg.inv(S) @ b
+            for j in range(0,2*jmax+1):
+                D[i,(i-jmax+j+grid.N)%grid.N] = a[j]
+        self.matrix = D
         pass
